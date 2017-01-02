05bdec2a3d13e8b839c1d3e57d0e37902417c81d
smaster0517 <smaster0517@gmail.com>
2017-01-02 19:19:01 +0200

    changed api endpoint

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
-const azureUrl = "http://estatewebapi.azurewebsites.net"
+const myaspUrl = "http://alexandrudanpop1-001-site2.btempurl.com/"
 const localUrl = "http://localhost:5000"
 
 export const api = window.location.href.indexOf("localhost") !== -1
   ? localUrl
-  : azureUrl
+  : myaspUrl
