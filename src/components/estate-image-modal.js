090a11070ef0cb0f4387c7d83b9f61032ecda07d
smaster0517 <smaster0517@gmail.com>
2017-01-21 17:27:42 +0200

    validations if file uploaded in not of img supported type

--- src/components/estate-image-modal.js
+++ src/components/estate-image-modal.js
@@ -1,4 +1,5 @@
 import styles from './modalStyles.scss'
+import commonStyles from './index.scss'
 import React from 'react'
 
 export default class EstateImageModal extends React.Component {
@@ -7,12 +8,30 @@ export default class EstateImageModal extends React.Component {
 
     this.state = {
       image: null,
-      estateId: null
+      estateId: null,
+      error: null
     }
+  }
+
+  isValidImage(fileName) {
+    const regex = /(?:\.([^.]+))?$/
+    const extension = regex.exec(fileName)[0].toLowerCase()
+
+    if (extension === '.jpg' || extension === '.png' || extension === '.bmp' || extension === '.gif' || extension === '.tif') {
+      return true
+    }
+    return false
   }
 
   onImageSelectionChange(estateId) {
     var file = this.refs.fileInput.files[0]
+
+    if (!file || !this.isValidImage(file.name)) {
+      this.setState({ error: "Provided file is not an image" })
+      return
+    } else {
+      this.setState({ error: null })
+    }
 
     let reader = new FileReader()
     reader.onloadend = () => {
@@ -40,6 +59,9 @@ export default class EstateImageModal extends React.Component {
         <div className={styles['modal-content']}>
           <span ref='closeButton' onClick={this.closeModal.bind(this)} className={styles['close']}>&times;</span>
           <h3> Add some images </h3>
+          <div className={commonStyles["error"]}>
+            {this.state.error}
+          </div>
           <button onClick={this.props.saveImage.bind(this, this.state.image, this.state.estateId)}> Save </button>
           <input className={styles['inputfile']} type='file' ref='fileInput' onChange={this.onImageSelectionChange.bind(this, this.props.estateId)} />
           <img className={styles['uploadedImage']} ref='imageContainer' />
